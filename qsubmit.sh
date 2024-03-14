6e8dbc2750d308d69f217e5c837c187d272f5104
Nils Strand <nstrand@uchicago.edu>
2024-03-14 08:30:24 -0500

    small change

--- qsubmit.sh
+++ qsubmit.sh
@@ -4,20 +4,20 @@
 #SBATCH --output=%x.out
 #SBATCH --error=%x.err
  
-#SBATCH --time=2-0:00:00
+#SBATCH --time=2:00:00
  
 #SBATCH --partition=dinner
 #SBATCH --account=pi-dinner
  
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=32
+#SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=10G
  
 #SBATCH --export=NONE
 
 export I_MPI_HYDRA_TOPOLIB=ipl
-export JULIA_NUM_THREADS=32
+export JULIA_NUM_THREADS=1
 
 echo $SLURM_JOB_NAME
 echo $SLURM_JOB_NODELIST
