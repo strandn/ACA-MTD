0f78ab6de92633b3d06477749de87b351c20d075
Nils Strand <nstrand@uchicago.edu>
2024-04-02 09:58:09 -0500

    changes to analysis file

--- gaussian_mtd_analysis.jl
+++ gaussian_mtd_analysis.jl
@@ -7,22 +7,21 @@ sigma = [0.15, 0.15]
 stride = 1000
 
 strlist = readlines("hills_$biasfactor.txt")
-data = [parse.(split(replace(str, r"[^\d\.-]+" => " "))) for str in strlist]
-len = length(data[:, 1])
+data = [parse.(Float64, split(replace(str, r"[^\d\.-]+" => " "))) for str in strlist]
 
-slist = data[:, 2:3]
-w = data[:, 4]
+slist = [line[2:3] for line in data]
+w = [line[4] for line in data]
 
 open("fes_$biasfactor.txt", "w") do file
 	result = zeros(nbins, nbins)
-	for i in 1:len
+	for i in eachindex(slist)
 		for j in 1:nbins
 			for k in 1:nbins
 				x = domain_cv[1][1] + (j - 1) * (domain_cv[1][2] - domain_cv[1][1]) / (nbins - 1)
 				y = domain_cv[2][1] + (k - 1) * (domain_cv[2][2] - domain_cv[2][1]) / (nbins - 1)
-				result[j, k] += w[i] * exp(-(x - slist[i, 1]) ^ 2 / (2 * sigma[1] ^ 2) - (y - slist[i, 2]) ^ 2 / (2 * sigma[2] ^ 2))
+				result[j, k] += w[i] * exp(-(x - slist[i][1]) ^ 2 / (2 * sigma[1] ^ 2) - (y - slist[i][2]) ^ 2 / (2 * sigma[2] ^ 2))
 				if i % stride == 0
-					write(file, "$(result[j, k]) ")
+					write(file, "$(-result[j, k]) ")
 				end
 			end
 			if i % stride == 0
