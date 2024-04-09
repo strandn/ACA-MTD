59fbaa0e9cd3ac8c50de633c8d1f82d5b3169ca0
Nils Strand <nstrand@uchicago.edu>
2024-04-09 15:26:35 -0500

    change aca acceptance ratio again

--- tt_aca.jl
+++ tt_aca.jl
@@ -244,9 +244,9 @@ function max_metropolis(F::ResFunc{T, N}, pivot::Vector{T}, n_samples::Int64, ju
         arg_new = [pivot; [p_new[k] for k in 1:order]]
         f_old = abs(F(arg_old...))
         f_new = abs(F(arg_new...))
-        acceptance_prob = min(1, f_new / f_old)
+        # acceptance_prob = min(1, f_new / f_old)
         # TODO might need to allow for variable kT
-        # acceptance_prob = min(1, exp(f_new - f_old))
+        acceptance_prob = min(1, exp(f_new - f_old))
         
         if isnan(acceptance_prob) || rand() < acceptance_prob
             chain[i, :] = p_new
