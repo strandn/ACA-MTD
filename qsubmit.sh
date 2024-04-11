3d52a8ef657af270db5a26d2160a1e2101016dea
Nils Strand <nstrand@midway3-login3.rcc.local>
2024-04-11 10:46:17 -0500

    trying kde with 1e6 samples

--- qsubmit.sh
+++ qsubmit.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
  
-#SBATCH --job-name=gaussian_knn
-#SBATCH --output=%x_%j.out
-#SBATCH --error=%x_%j.err
+#SBATCH --job-name=gaussian_biased_1e6
+#SBATCH --output=%x.out
+#SBATCH --error=%x.err
+
+##SBATCH --job-name=gaussian_knn
+##SBATCH --output=%x_%j.out
+##SBATCH --error=%x_%j.err
  
 #SBATCH --time=2-0:00:00
  
@@ -10,7 +14,7 @@
 #SBATCH --account=pi-dinner
  
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=10
+#SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=10G
  
@@ -23,6 +27,6 @@ echo $SLURM_JOB_NAME
 echo $SLURM_JOB_NODELIST
 
 # julia gaussian_cv.jl
-# mpiexecjl -n 1 julia gaussian_biased.jl $RANK $K
+mpiexecjl -n 1 julia gaussian_biased.jl
 # julia gaussian_mtd.jl 20
-mpiexecjl -n 10 julia gaussian_knn.jl $RANK $K
+# mpiexecjl -n 10 julia gaussian_knn.jl $RANK $K
