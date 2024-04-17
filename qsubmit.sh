9b096d38ff3795bb2c528ffd46826217bbabb2af
Nils Strand <nstrand@midway3-login3.rcc.local>
2024-04-17 16:13:50 -0500

    testing new version

--- qsubmit.sh
+++ qsubmit.sh
@@ -4,7 +4,7 @@
 ##SBATCH --output=%x.out
 ##SBATCH --error=%x.err
 
-#SBATCH --job-name=gaussian_cv
+#SBATCH --job-name=gaussian_knn
 #SBATCH --output=%x_%j.out
 #SBATCH --error=%x_%j.err
  
@@ -14,19 +14,19 @@
 #SBATCH --account=pi-dinner
  
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=48
-#SBATCH --mem=100G
+#SBATCH --ntasks-per-node=10
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=10G
  
 #SBATCH --export=NONE
 
 export I_MPI_HYDRA_TOPOLIB=ipl
-export JULIA_NUM_THREADS=48
+export JULIA_NUM_THREADS=1
 
 echo $SLURM_JOB_NAME
 echo $SLURM_JOB_NODELIST
 
-julia gaussian_cv.jl
+# julia gaussian_cv.jl
 # mpiexecjl -n 1 julia gaussian_biased.jl
 # julia gaussian_mtd.jl 20
-# mpiexecjl -n 10 julia gaussian_knn.jl $RANK $K
+mpiexecjl -n 10 julia gaussian_knn.jl $RANK $K
