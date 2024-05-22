1ffe15e62ac6cf98c5a97dcdd27bddd283e61bbb
Nils Strand <nstrand@uchicago.edu>
2024-05-22 15:53:50 -0500

    changes to well tempered metadynamics code

--- gaussian_mtd.jl
+++ gaussian_mtd.jl
@@ -21,25 +21,26 @@ function V(r)
 		(x1 + 1/3) ^ 4 / 5 + (x2 + 2/3) ^ 4 / 5 + x3 ^ 4 / 5 + (x4 + 1/3) ^ 4 / 5
 end
 
-function Vbias(r, slist, w, sigma)
+function Vbias(r, slist, w, sigma, domain)
 	result = 0.0
 	for i in eachindex(slist)
-		result += w[i] * exp(-(x(r) - slist[i][1]) ^ 2 / (2 * sigma[1] ^ 2) - (y(r) - slist[i][2]) ^ 2 / (2 * sigma[2] ^ 2))
+		result += w[i] * exp(-(x(r) - slist[i][1]) ^ 2 / (2 * (sigma * (domain[1][2] - domain[1][1])) ^ 2) - (y(r) - slist[i][2]) ^ 2 / (2 * (sigma * (domain[2][2] - domain[2][1])) ^ 2))
 	end
 	return result
 end
 
-function grad_V(r, slist, w, sigma)
+function grad_V(r, slist, w, sigma, domain)
 	grad = ForwardDiff.gradient(V, r)
 	for i in eachindex(slist)
-		Vinc(r) = w[i] * exp(-(x(r) - slist[i][1]) ^ 2 / (2 * sigma[1] ^ 2) - (y(r) - slist[i][2]) ^ 2 / (2 * sigma[2] ^ 2))
+		Vinc(r) = w[i] * exp(-(x(r) - slist[i][1]) ^ 2 / (2 * (sigma * (domain[1][2] - domain[1][1])) ^ 2) - (y(r) - slist[i][2]) ^ 2 / (2 * (sigma * (domain[2][2] - domain[2][1])) ^ 2))
 		grad += ForwardDiff.gradient(Vinc, r)
 	end
 	return grad
 end
 
-function well_tempered_mtd(biasfactor)
-	domain = ((-2.0, 2.0), (-2.0, 2.0), (-2.0, 2.0), (-2.0, 2.0))
+function well_tempered_mtd()
+	domain = [(-2.0, 2.0), (-2.0, 2.0), (-2.0, 2.0), (-2.0, 2.0)]
+	domain_cv = [(-1.5, 4.0), (-1.5, 4.5)]
 	
 	T = 1.0
 	gamma = 1.0
@@ -59,18 +60,17 @@ function well_tempered_mtd(biasfactor)
 	t = 0.0
 
 	pace = 500
-	height = 1.2
-	# biasfactor = 6
-	sigma = [0.15, 0.15]
+	height = 1.0
+	# sigma = [0.15, 0.15]
 	
 	traj = []
 	slist = []
 	w = []
-	if isfile("hills_$biasfactor.txt")
-		rm("hills_$biasfactor.txt")
+	if isfile("hills_$(biasfactor)_$(sigma).txt")
+		rm("hills_$(biasfactor)_$(sigma).txt")
 	end
 	for i in 1:steps
-		grad = grad_V([x1, x2, x3, x4], slist, w, sigma)
+		grad = grad_V([x1, x2, x3, x4], slist, w, sigma, domain_cv)
 	
 		v1 = -(grad[1] / gamma) + rand(normal_dist)
 		v2 = -(grad[2] / gamma) + rand(normal_dist)
@@ -101,22 +101,22 @@ function well_tempered_mtd(biasfactor)
 		end
 	
 		if i % stride == 0
-			push!(traj, (t, x([x1, x2, x3, x4]), y([x1, x2, x3, x4]), Vbias([x1, x2, x3, x4], slist, w, sigma)))
+			push!(traj, (t, x([x1, x2, x3, x4]), y([x1, x2, x3, x4]), Vbias([x1, x2, x3, x4], slist, w, sigma, domain_cv)))
 		end
 
 		if i % pace == 0
 			s_new = [x([x1, x2, x3, x4]), y([x1, x2, x3, x4])]
-			w_new = height * exp(-Vbias([x1, x2, x3, x4], slist, w, sigma) / (kb * T * (biasfactor - 1)))
+			w_new = height * exp(-Vbias([x1, x2, x3, x4], slist, w, sigma, domain_cv) / (kb * T * (biasfactor - 1)))
 			push!(slist, s_new)
 			push!(w, w_new)
-			open("hills_$biasfactor.txt", "a") do file
+			open("hills_$(biasfactor)_$(sigma).txt", "a") do file
 				write(file, "$t $s_new $w_new\n")
 			end
 		end
 
 		t += dt
 	end
-	open("colvar_$biasfactor.txt", "w") do file
+	open("colvar_$(biasfactor)_$(sigma).txt", "w") do file
 		for step in traj
 			write(file, "$(step[1]) $(step[2]) $(step[3]) $(step[4])\n")
 		end
@@ -131,4 +131,5 @@ end
 println(ARGS)
 flush(stdout)
 biasfactor = parse(Int64, ARGS[1])
-well_tempered_mtd(biasfactor)
+sigma = parse(Int64, ARGS[2])
+well_tempered_mtd()
