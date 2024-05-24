0a4739d6f636b9fa935b2ed4e3fa6370426ceeb4
Nils Strand <nstrand@uchicago.edu>
2024-05-23 22:03:20 -0500

    bug in marginal code

--- gaussian_marginals.jl
+++ gaussian_marginals.jl
@@ -26,81 +26,112 @@ function gaussian_marginals()
 	flush(stdout)
 
 	nbins = 1000
-	ranges = [LinRange(d[1], d[2], nbins) for d in domain]
+	# ranges = [LinRange(d[1], d[2], nbins) for d in domain]
 
+	G = zeros(nbins)
 	rho_total = 0.0
 	println("Computing F(x1)...")
 	flush(stdout)
+	Threads.@threads for i in nbins
+		dx = (domain[1][2] - domain[1][1]) / (nbins - 1)
+		x = (i - 1) * dx + domain[1][1]
+		f(x2, x3, x4) = P(x, x2, x3, x4)
+		rho, _ = hcubature(x->f(x[1], x[2], x[3]), [domain[k][1] for k in [2, 3, 4]], [domain[k][2] for k in [2, 3, 4]])
+		rho_total += rho * dx
+		G[i] = -log(rho) / beta
+	end
 	open("fes_correct_1.txt", "w") do file
-		Threads.@threads for x in ranges[1]
-			f(x2, x3, x4) = P(x, x2, x3, x4)
-			rho, _ = hcubature(x->f(x[1], x[2], x[3]), [domain[k][1] for k in [2, 3, 4]], [domain[k][2] for k in [2, 3, 4]])
-			rho_total += rho * (domain[1][2] - domain[1][1]) / (nbins - 1)
-			write(file, "$(-log(rho) / beta) ")
+		for i in nbins
+			write(file, "$(G[i]) ")
 		end
 		write(file, "\n")
 	end
 	println(rho_total)
 	println()
 
+	G = zeros(nbins)
 	rho_total = 0.0
 	println("Computing F(x2)...")
 	flush(stdout)
+	Threads.@threads for i in nbins
+		dx = (domain[2][2] - domain[2][1]) / (nbins - 1)
+		x = (i - 1) * dx + domain[2][1]
+		f(x1, x3, x4) = P(x1, x, x3, x4)
+		rho, _ = hcubature(x->f(x[1], x[2], x[3]), [domain[k][1] for k in [1, 3, 4]], [domain[k][2] for k in [1, 3, 4]])
+		rho_total += rho * dx
+		G[i] = -log(rho) / beta
+	end
 	open("fes_correct_2.txt", "w") do file
-		Threads.@threads for x in ranges[2]
-			f(x1, x3, x4) = P(x1, x, x3, x4)
-			rho, _ = hcubature(x->f(x[1], x[2], x[3]), [domain[k][1] for k in [1, 3, 4]], [domain[k][2] for k in [1, 3, 4]])
-			rho_total += rho * (domain[2][2] - domain[2][1]) / (nbins - 1)
-			write(file, "$(-log(rho) / beta) ")
+		for i in nbins
+			write(file, "$(G[i]) ")
 		end
 		write(file, "\n")
 	end
 	println(rho_total)
 	println()
 
+	G = zeros(nbins)
 	rho_total = 0.0
 	println("Computing F(x3)...")
 	flush(stdout)
+	Threads.@threads for i in nbins
+		dx = (domain[3][2] - domain[3][1]) / (nbins - 1)
+		x = (i - 1) * dx + domain[3][1]
+		f(x1, x2, x4) = P(x1, x2, x, x4)
+		rho, _ = hcubature(x->f(x[1], x[2], x[3]), [domain[k][1] for k in [1, 2, 4]], [domain[k][2] for k in [1, 2, 4]])
+		rho_total += rho * dx
+		G[i] = -log(rho) / beta
+	end
 	open("fes_correct_3.txt", "w") do file
-		Threads.@threads for x in ranges[3]
-			f(x1, x2, x4) = P(x1, x2, x, x4)
-			rho, _ = hcubature(x->f(x[1], x[2], x[3]), [domain[k][1] for k in [1, 2, 4]], [domain[k][2] for k in [1, 2, 4]])
-			rho_total += rho * (domain[3][2] - domain[3][1]) / (nbins - 1)
-			write(file, "$(-log(rho) / beta) ")
+		for i in nbins
+			write(file, "$(G[i]) ")
 		end
 		write(file, "\n")
 	end
 	println(rho_total)
 	println()
 
+	G = zeros(nbins)
 	rho_total = 0.0
 	println("Computing F(x4)...")
 	flush(stdout)
+	Threads.@threads for i in nbins
+		dx = (domain[4][2] - domain[4][1]) / (nbins - 1)
+		x = (i - 1) * dx + domain[4][1]
+		f(x1, x2, x3) = P(x1, x2, x3, x)
+		rho, _ = hcubature(x->f(x[1], x[2], x[3]), [domain[k][1] for k in [1, 2, 3]], [domain[k][2] for k in [1, 2, 3]])
+		rho_total += rho * dx
+		G[i] = -log(rho) / beta
+	end
 	open("fes_correct_4.txt", "w") do file
-		Threads.@threads for x in ranges[4]
-			f(x1, x2, x3) = P(x1, x2, x3, x)
-			rho, _ = hcubature(x->f(x[1], x[2], x[3]), [domain[k][1] for k in [1, 2, 3]], [domain[k][2] for k in [1, 2, 3]])
-			rho_total += rho * (domain[4][2] - domain[4][1]) / (nbins - 1)
-			write(file, "$(-log(rho) / beta) ")
+		for i in nbins
+			write(file, "$(G[i]) ")
 		end
 		write(file, "\n")
 	end
 	println(rho_total)
 	println()
 
-	nbins = 100
-	ranges = [LinRange(d[1], d[2], nbins) for d in domain]
-
+	G = zeros(nbins, nbins)
 	rho_total = 0.0
 	println("Computing F(x1, x2)...")
 	flush(stdout)
+	for i in nbins
+		dx = (domain[1][2] - domain[1][1]) / (nbins - 1)
+		x = (i - 1) * dx + domain[1][1]
+		Threads.@threads for j in nbins
+			dy = (domain[2][2] - domain[2][1]) / (nbins - 1)
+			y = (i - 1) * dy + domain[2][1]
+			f(x3, x4) = P(x, y, x3, x4)
+			rho, _ = hcubature(x->f(x[1], x[2]), [domain[k][1] for k in [3, 4]], [domain[k][2] for k in [3, 4]])
+			rho_total += rho * dx * dy
+			G[i, j] = -log(rho) / beta
+		end
+	end
 	open("fes_correct_12.txt", "w") do file
-		Threads.@threads for x in ranges[1]
-			for y in ranges[2]
-				f(x3, x4) = P(x, y, x3, x4)
-				rho, _ = hcubature(x->f(x[1], x[2]), [domain[k][1] for k in [3, 4]], [domain[k][2] for k in [3, 4]])
-				rho_total += rho * (domain[1][2] - domain[1][1]) * (domain[2][2] - domain[2][1]) / (nbins - 1) ^ 2
-				write(file, "$(-log(rho) / beta) ")
+		for i in nbins
+			for j in nbins
+				write(file, "$(G[i, j]) ")
 			end
 			write(file, "\n")
 		end
@@ -108,16 +139,26 @@ function gaussian_marginals()
 	println(rho_total)
 	println()
 
+	G = zeros(nbins, nbins)
 	rho_total = 0.0
 	println("Computing F(x1, x3)...")
 	flush(stdout)
+	for i in nbins
+		dx = (domain[1][2] - domain[1][1]) / (nbins - 1)
+		x = (i - 1) * dx + domain[1][1]
+		Threads.@threads for j in nbins
+			dy = (domain[3][2] - domain[3][1]) / (nbins - 1)
+			y = (i - 1) * dy + domain[3][1]
+			f(x2, x4) = P(x, x2, y, x4)
+			rho, _ = hcubature(x->f(x[1], x[2]), [domain[k][1] for k in [2, 4]], [domain[k][2] for k in [2, 4]])
+			rho_total += rho * dx * dy
+			G[i, j] = -log(rho) / beta
+		end
+	end
 	open("fes_correct_13.txt", "w") do file
-		Threads.@threads for x in ranges[1]
-			for y in ranges[3]
-				f(x2, x4) = P(x, x2, y, x4)
-				rho, _ = hcubature(x->f(x[1], x[2]), [domain[k][1] for k in [2, 4]], [domain[k][2] for k in [2, 4]])
-				rho_total += rho * (domain[1][2] - domain[1][1]) * (domain[3][2] - domain[3][1]) / (nbins - 1) ^ 2
-				write(file, "$(-log(rho) / beta) ")
+		for i in nbins
+			for j in nbins
+				write(file, "$(G[i, j]) ")
 			end
 			write(file, "\n")
 		end
@@ -125,16 +166,26 @@ function gaussian_marginals()
 	println(rho_total)
 	println()
 
+	G = zeros(nbins, nbins)
 	rho_total = 0.0
 	println("Computing F(x1, x4)...")
 	flush(stdout)
+	for i in nbins
+		dx = (domain[1][2] - domain[1][1]) / (nbins - 1)
+		x = (i - 1) * dx + domain[1][1]
+		Threads.@threads for j in nbins
+			dy = (domain[4][2] - domain[4][1]) / (nbins - 1)
+			y = (i - 1) * dy + domain[4][1]
+			f(x2, x3) = P(x, x2, x3, y)
+			rho, _ = hcubature(x->f(x[1], x[2]), [domain[k][1] for k in [2, 3]], [domain[k][2] for k in [2, 3]])
+			rho_total += rho * dx * dy
+			G[i, j] = -log(rho) / beta
+		end
+	end
 	open("fes_correct_14.txt", "w") do file
-		Threads.@threads for x in ranges[1]
-			for y in ranges[4]
-				f(x2, x3) = P(x, x2, x3, y)
-				rho, _ = hcubature(x->f(x[1], x[2]), [domain[k][1] for k in [2, 3]], [domain[k][2] for k in [2, 3]])
-				rho_total += rho * (domain[1][2] - domain[1][1]) * (domain[4][2] - domain[4][1]) / (nbins - 1) ^ 2
-				write(file, "$(-log(rho) / beta) ")
+		for i in nbins
+			for j in nbins
+				write(file, "$(G[i, j]) ")
 			end
 			write(file, "\n")
 		end
@@ -142,16 +193,26 @@ function gaussian_marginals()
 	println(rho_total)
 	println()
 
+	G = zeros(nbins, nbins)
 	rho_total = 0.0
 	println("Computing F(x2, x3)...")
 	flush(stdout)
+	for i in nbins
+		dx = (domain[2][2] - domain[2][1]) / (nbins - 1)
+		x = (i - 1) * dx + domain[2][1]
+		Threads.@threads for j in nbins
+			dy = (domain[3][2] - domain[3][1]) / (nbins - 1)
+			y = (i - 1) * dy + domain[3][1]
+			f(x1, x4) = P(x1, x, y, x4)
+			rho, _ = hcubature(x->f(x[1], x[2]), [domain[k][1] for k in [1, 4]], [domain[k][2] for k in [1, 4]])
+			rho_total += rho * dx * dy
+			G[i, j] = -log(rho) / beta
+		end
+	end
 	open("fes_correct_23.txt", "w") do file
-		Threads.@threads for x in ranges[2]
-			for y in ranges[3]
-				f(x1, x4) = P(x1, x, y, x4)
-				rho, _ = hcubature(x->f(x[1], x[2]), [domain[k][1] for k in [1, 4]], [domain[k][2] for k in [1, 4]])
-				rho_total += rho * (domain[2][2] - domain[2][1]) * (domain[3][2] - domain[3][1]) / (nbins - 1) ^ 2
-				write(file, "$(-log(rho) / beta) ")
+		for i in nbins
+			for j in nbins
+				write(file, "$(G[i, j]) ")
 			end
 			write(file, "\n")
 		end
@@ -159,16 +220,26 @@ function gaussian_marginals()
 	println(rho_total)
 	println()
 
+	G = zeros(nbins, nbins)
 	rho_total = 0.0
 	println("Computing F(x2, x4)...")
 	flush(stdout)
+	for i in nbins
+		dx = (domain[2][2] - domain[2][1]) / (nbins - 1)
+		x = (i - 1) * dx + domain[2][1]
+		Threads.@threads for j in nbins
+			dy = (domain[4][2] - domain[4][1]) / (nbins - 1)
+			y = (i - 1) * dy + domain[4][1]
+			f(x1, x3) = P(x1, x, x3, y)
+			rho, _ = hcubature(x->f(x[1], x[2]), [domain[k][1] for k in [1, 3]], [domain[k][2] for k in [1, 3]])
+			rho_total += rho * dx * dy
+			G[i, j] = -log(rho) / beta
+		end
+	end
 	open("fes_correct_24.txt", "w") do file
-		Threads.@threads for x in ranges[2]
-			for y in ranges[4]
-				f(x1, x3) = P(x1, x, x3, y)
-				rho, _ = hcubature(x->f(x[1], x[2]), [domain[k][1] for k in [1, 3]], [domain[k][2] for k in [1, 3]])
-				rho_total += rho * (domain[2][2] - domain[2][1]) * (domain[4][2] - domain[4][1]) / (nbins - 1) ^ 2
-				write(file, "$(-log(rho) / beta) ")
+		for i in nbins
+			for j in nbins
+				write(file, "$(G[i, j]) ")
 			end
 			write(file, "\n")
 		end
@@ -176,16 +247,26 @@ function gaussian_marginals()
 	println(rho_total)
 	println()
 
+	G = zeros(nbins, nbins)
 	rho_total = 0.0
 	println("Computing F(x3, x4)...")
 	flush(stdout)
+	for i in nbins
+		dx = (domain[3][2] - domain[3][1]) / (nbins - 1)
+		x = (i - 1) * dx + domain[3][1]
+		Threads.@threads for j in nbins
+			dy = (domain[4][2] - domain[4][1]) / (nbins - 1)
+			y = (i - 1) * dy + domain[4][1]
+			f(x1, x2) = P(x1, x2, x, y)
+			rho, _ = hcubature(x->f(x[1], x[2]), [domain[k][1] for k in [1, 2]], [domain[k][2] for k in [1, 2]])
+			rho_total += rho * dx * dy
+			G[i, j] = -log(rho) / beta
+		end
+	end
 	open("fes_correct_34.txt", "w") do file
-		Threads.@threads for x in ranges[3]
-			for y in ranges[4]
-				f(x1, x2) = P(x1, x2, x, y)
-				rho, _ = hcubature(x->f(x[1], x[2]), [domain[k][1] for k in [1, 2]], [domain[k][2] for k in [1, 2]])
-				rho_total += rho * (domain[3][2] - domain[3][1]) * (domain[4][2] - domain[4][1]) / (nbins - 1) ^ 2
-				write(file, "$(-log(rho) / beta) ")
+		for i in nbins
+			for j in nbins
+				write(file, "$(G[i, j]) ")
 			end
 			write(file, "\n")
 		end
