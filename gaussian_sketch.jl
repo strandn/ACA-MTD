11b22a7a7ff34d8caf04d65d9095e714e1cb27c3
Nils Strand <nstrand@uchicago.edu>
2024-05-23 19:12:32 -0500

    change back to 0.02

--- gaussian_sketch.jl
+++ gaussian_sketch.jl
@@ -61,7 +61,7 @@ function update_conv(basis, basislist, basisdlist, domain, nbins, nbasis)
 		gridpoints_d = [[0.0 for _ in 1:nbins] for _ in 1:nbasis]
 		for j in 1:nbasis
 			for k in 1:nbins
-				w = 0.01
+				w = 0.02
 				sigma = w * (domain[i][2] - domain[i][1])
 				s = domain[i][1] + (k - 1) * (domain[i][2] - domain[i][1]) / (nbins - 1)
 				f(x) = basis[i](x, j) * (1 / (sqrt(2 * pi) * sigma)) * exp(-(s - x) ^ 2 / (2 * sigma ^ 2))
