b2bb38a42da6eb0bb9e83030d4523d08f074c567
Nils Strand <nstrand@midway3-login3.rcc.local>
2024-05-23 23:28:53 -0500

    hopefully marginal better now

--- qsubmit.sh
+++ qsubmit.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
  
-##SBATCH --job-name=gaussian_cv
-##SBATCH --output=%x.out
-##SBATCH --error=%x.err
+#SBATCH --job-name=gaussian_cv
+#SBATCH --output=%x.out
+#SBATCH --error=%x.err
 
-#SBATCH --job-name=gaussian_sketch
-#SBATCH --output=%x_%j.out
-#SBATCH --error=%x_%j.err
+##SBATCH --job-name=gaussian_sketch
+##SBATCH --output=%x_%j.out
+##SBATCH --error=%x_%j.err
  
 #SBATCH --time=2-0:00:00
  
@@ -15,13 +15,13 @@
  
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=1
-#SBATCH --mem=10G
+#SBATCH --cpus-per-task=48
+#SBATCH --mem=100G
  
 #SBATCH --export=NONE
 
 export I_MPI_HYDRA_TOPOLIB=ipl
-export JULIA_NUM_THREADS=1
+export JULIA_NUM_THREADS=48
 
 echo $SLURM_JOB_NAME
 echo $SLURM_JOB_NODELIST
@@ -30,5 +30,5 @@ echo $SLURM_JOB_NODELIST
 # mpiexecjl -n 1 julia gaussian_biased.jl
 # julia gaussian_mtd.jl 20
 # mpiexecjl -n 10 julia gaussian_knn.jl $RANK $K
-julia gaussian_sketch.jl $R $RC $NBASIS
-# julia gaussian_marginals.jl
+# julia gaussian_sketch.jl $R $RC $NBASIS
+julia gaussian_marginals.jl
