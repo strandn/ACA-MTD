e53f5fb8bed53e5deda636744b20d4bf7e60c026
Nils Strand <nstrand@uchicago.edu>
2024-05-30 14:34:03 -0500

    minor

--- gaussian_4d.jl
+++ gaussian_4d.jl
@@ -211,7 +211,7 @@ function sketch_mtd()
 		ranges = [LinRange(d[1], d[2], nbins) for d in domain]
 		for i in 1:4
 			bw = 0.01 .* (domain[i][2] - domain[i][1])
-			kde_result = kde([step[i] for step in traj], npoints = nbins, weights = weights / sum(weights), bandwidth = bw)
+			kde_result = kde([step[i] for step in samples], npoints = nbins, weights = weights / sum(weights), bandwidth = bw)
 			ik = InterpKDE(kde_result)
 			open("data/kde_$(i)_$(count)_$(r)_$(rc)_$(nbasis)_$(nsamples).txt", "w") do file
 				for x in ranges[i]
@@ -226,7 +226,7 @@ function sketch_mtd()
 		for i in 1:4
 			for j in i+1:4
 				bw = 0.02 .* (domain[i][2] - domain[i][1], domain[j][2] - domain[j][1])
-				kde_result = kde(hcat([step[i] for step in traj], [step[j] for step in traj]), npoints = (nbins, nbins), weights = weights / sum(weights), bandwidth = bw)
+				kde_result = kde(hcat([step[i] for step in samples], [step[j] for step in samples]), npoints = (nbins, nbins), weights = weights / sum(weights), bandwidth = bw)
 				ik = InterpKDE(kde_result)
 				open("data/kde_$(i)$(j)_$(count)_$(r)_$(rc)_$(nbasis)_$(nsamples).txt", "w") do file
 					for x in ranges[i]
