6948f9f4874734ff12ee15c059d8fe94da2b50b5
Nils Strand <nstrand@midway3-login3.rcc.local>
2024-06-01 16:21:15 -0500

    results for 0.04

--- qsubmit.sh
+++ qsubmit.sh
@@ -20,7 +20,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=10G
+#SBATCH --mem=20G
  
 #SBATCH --export=NONE
 
@@ -36,4 +36,4 @@ echo $SLURM_JOB_NODELIST
 # mpiexecjl -n 10 julia gaussian_knn.jl $RANK $K
 # julia gaussian_sketch.jl $R $RC $NBASIS 5
 # julia gaussian_marginals.jl
-julia gaussian_4d.jl $R $RC $NBASIS 2
+julia gaussian_4d.jl $R $RC $NBASIS 1
