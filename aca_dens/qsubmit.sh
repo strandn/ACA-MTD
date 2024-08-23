4a374690370d017dc3339a684cdf53a210232471
Nils Strand <nstrand@midway3-login3.rcc.local>
2024-08-23 16:22:32 -0500

    lets see if 4d is working

--- aca_dens/qsubmit.sh
+++ aca_dens/qsubmit.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
  
-#SBATCH --job-name=gaussian_acadens
+##SBATCH --job-name=gaussian_acadens
+#SBATCH --job-name=gaussian_4d
 #SBATCH --output=%x_%j.out
 #SBATCH --error=%x_%j.err
  
@@ -12,7 +13,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=8
-#SBATCH --mem=20G
+#SBATCH --mem=100G
  
 #SBATCH --export=NONE
 
@@ -21,4 +22,5 @@ export JULIA_NUM_THREADS=8
 echo $SLURM_JOB_NAME
 echo $SLURM_JOB_NODELIST
 
-julia gaussian_2d.jl $RC $NBASIS $NSAMPLES
+#julia gaussian_2d.jl $RC $NBASIS $NSAMPLES
+julia gaussian_4d.jl $RC $NBASIS $NSAMPLES
