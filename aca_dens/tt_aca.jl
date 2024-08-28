f4ddfcb0c5cf295f5888585744015416d92a2ecd
Nils Strand <nstrand@uchicago.edu>
2024-08-27 22:33:45 -0500

    relax aca cutoff

--- aca_dens/tt_aca.jl
+++ aca_dens/tt_aca.jl
@@ -147,7 +147,7 @@ function update_vb(vb::MPS, G::MPS, basis, convbasis, n::Int64, domain::Vector{T
     else
         dens_eval(vb, convbasis, [elt for elt in x]) + kT * log(max(dens_eval(G, convbasis, [elt for elt in x]), 1))
     end
-    F = ResFunc(P, Tuple(domain), 1.0e-10)
+    F = ResFunc(P, Tuple(domain), 0.01)
 
     println()
     println("Starting TT-cross ACA...")
