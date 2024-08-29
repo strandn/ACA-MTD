41b031dab519d2ccdced4e6bf87f453a3e2ef40c
Nils Strand <nstrand@uchicago.edu>
2024-08-29 00:20:04 -0500

    try previously used P(x) for aca (5kT)

--- aca_dens/tt_aca.jl
+++ aca_dens/tt_aca.jl
@@ -78,11 +78,16 @@ end
 function update_vb(vb::MPS, G::MPS, basis, convbasis, n::Int64, domain::Vector{Tuple{Float64, Float64}}, samples, kT, vshift::Float64)
     d = length(basis)
     P(x...) = if length(vb) == 0
-        kT * log(max(dens_eval(G, convbasis, [elt for elt in x]), 0.1))
+        kT * log(max(dens_eval(G, convbasis, [elt for elt in x]), 1))
     else
-        max(dens_eval(vb, convbasis, [elt for elt in x]) + kT * log(max(dens_eval(G, convbasis, [elt for elt in x]), 1)) - vshift, -2 * kT)
+        max(dens_eval(vb, convbasis, [elt for elt in x]) - vshift, -5 * kT) + kT * log(max(dens_eval(G, convbasis, [elt for elt in x]), 1))
     end
-    F = ResFunc(P, Tuple(domain), 0.05)
+    # P(x...) = if length(vb) == 0
+    #     kT * log(max(dens_eval(G, convbasis, [elt for elt in x]), 0.1))
+    # else
+    #     max(dens_eval(vb, convbasis, [elt for elt in x]) + kT * log(max(dens_eval(G, convbasis, [elt for elt in x]), 1)) - vshift, -2 * kT)
+    # end
+    F = ResFunc(P, Tuple(domain), 0.02)
 
     println()
     println("Starting TT-cross ACA...")
