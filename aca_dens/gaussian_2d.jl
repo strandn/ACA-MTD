2435e681a1ea1e9e0dffc6e0487fba14da287275
Nils Strand <nstrand@uchicago.edu>
2024-08-29 20:39:16 -0500

    actually first try with accurate int

--- aca_dens/gaussian_2d.jl
+++ aca_dens/gaussian_2d.jl
@@ -219,9 +219,9 @@ function sketch_mtd()
 		println("Vtop = $vpeak Vshift = $vshift")
 		flush(stdout)
 
-		# vb = update_vb(vb, G, basis, convbasis, nbasis, domain_cv, samples, kb * T, vshift)
-		sampleinc = div(length(samples) - 1, maxsamples) + 1
-		vb = update_vb(vb, G, basis, convbasis, nbasis, domain_cv, samples[1:sampleinc:length(samples)], kb * T, vshift)
+		vb = update_vb(vb, G, basis, convbasis, nbasis, domain_cv, samples, kb * T, vshift)
+		# sampleinc = div(length(samples) - 1, maxsamples) + 1
+		# vb = update_vb(vb, G, basis, convbasis, nbasis, domain_cv, samples[1:sampleinc:length(samples)], kb * T, vshift)
 
 		gradpeak = gradtop(vb, convbasis, convbasisd, samples)
 		println("\nmaxgrad = $gradpeak")
