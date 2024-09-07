db3ab648f633a0770d505d5cc9fe8c5b574564f5
Nils Strand <nstrand@midway3-login3.rcc.local>
2024-09-07 08:41:23 -0500

    check vbias

--- aladip_mtd/qsubmit.sh
+++ aladip_mtd/qsubmit.sh
@@ -21,3 +21,4 @@ echo $SLURM_JOB_NODELIST
 
 rm -f analysis.* \#md_0_1.* bck.*
 gmx_mpi mdrun -deffnm md_0_1 -plumed plumed2.dat -pin off
+# gmx_mpi mdrun -deffnm md_0_1 -plumed plumed3.dat -pin off
