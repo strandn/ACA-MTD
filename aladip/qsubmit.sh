8a2a0095b734e76df3412cfe5a4b4e7ab4247466
Nils Strand <nstrand@midway3-login3.rcc.local>
2024-09-07 08:22:23 -0500

    add md stuff to aca-mtd repository

---
+++ aladip/qsubmit.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+ 
+#SBATCH --job-name=aladip_vacuum_ttsketch
+#SBATCH --output=%x.out
+#SBATCH --error=%x.err
+
+#SBATCH --time=2-0:00:00
+ 
+#SBATCH --partition=dinner
+#SBATCH --account=pi-dinner
+ 
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=100G
+ 
+#SBATCH --export=NONE
+
+echo $SLURM_JOB_NAME
+echo $SLURM_JOB_NODELIST
+
+#gmx_mpi mdrun -deffnm md_0_1 -plumed plumed3.dat -ntomp 4
+rm -f analysis.* \#md_0_1.* bck.*
+gmx_mpi mdrun -deffnm md_0_1 -plumed plumed6.dat -pin off
