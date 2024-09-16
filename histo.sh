1f0a499d69a0203f9d3bcbd9b370e195ae4cdbd8
Nils Strand <nstrand@midway3-login3.rcc.local>
2024-09-16 10:29:26 -0500

    more samples for histo improves accuracy

---
+++ histo.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+cd ala3_20_20
+rm -f analysis.*
+plumed driver --plumed plumed2.dat --noatoms
+cd ..
+cd ala3_40_20
+rm -f analysis.*
+plumed driver --plumed plumed2.dat --noatoms
+cd ..
+cd ala3_30_30
+rm -f analysis.*
+plumed driver --plumed plumed2.dat --noatoms
+cd ..
+cd ala3_60_30
+rm -f analysis.*
+plumed driver --plumed plumed2.dat --noatoms
+cd ..
+cd ala3_40_40
+rm -f analysis.*
+plumed driver --plumed plumed2.dat --noatoms
+cd ..
+cd ala3_80_40
+rm -f analysis.*
+plumed driver --plumed plumed2.dat --noatoms
+cd ..
+cd ala3_20_20_wt
+rm -f analysis.*
+plumed driver --plumed plumed2.dat --noatoms
+cd ..
+cd ala3_40_20_wt
+rm -f analysis.*
+plumed driver --plumed plumed2.dat --noatoms
+cd ..
+cd ala3_30_30_wt
+rm -f analysis.*
+plumed driver --plumed plumed2.dat --noatoms
+cd ..
+cd ala3_60_30_wt
+rm -f analysis.*
+plumed driver --plumed plumed2.dat --noatoms
+cd ..
+cd ala3_40_40_wt
+rm -f analysis.*
+plumed driver --plumed plumed2.dat --noatoms
+cd ..
+cd ala3_80_40_wt
+rm -f analysis.*
+plumed driver --plumed plumed2.dat --noatoms
+cd ..
+cd ala3_20_20_10
+rm -f analysis.*
+plumed driver --plumed plumed2.dat --noatoms
+cd ..
+cd ala3_40_20_10
+rm -f analysis.*
+plumed driver --plumed plumed2.dat --noatoms
+cd ..
+cd ala3_30_30_10
+rm -f analysis.*
+plumed driver --plumed plumed2.dat --noatoms
+cd ..
+cd ala3_60_30_10
+rm -f analysis.*
+plumed driver --plumed plumed2.dat --noatoms
+cd ..
+cd ala3_40_40_10
+rm -f analysis.*
+plumed driver --plumed plumed2.dat --noatoms
+cd ..
+cd ala3_80_40_10
+rm -f analysis.*
+plumed driver --plumed plumed2.dat --noatoms
+cd ..
+cd ala3_20_20_10_wt
+rm -f analysis.*
+plumed driver --plumed plumed2.dat --noatoms
+cd ..
+cd ala3_40_20_10_wt
+rm -f analysis.*
+plumed driver --plumed plumed2.dat --noatoms
+cd ..
+cd ala3_30_30_10_wt
+rm -f analysis.*
+plumed driver --plumed plumed2.dat --noatoms
+cd ..
+cd ala3_60_30_10_wt
+rm -f analysis.*
+plumed driver --plumed plumed2.dat --noatoms
+cd ..
+cd ala3_40_40_10_wt
+rm -f analysis.*
+plumed driver --plumed plumed2.dat --noatoms
+cd ..
+cd ala3_80_40_10_wt
+rm -f analysis.*
+plumed driver --plumed plumed2.dat --noatoms
+cd ..
