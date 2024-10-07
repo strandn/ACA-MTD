a028bf57f0058fc7224c292b2bd6c55c51156600
Nils Strand <nstrand@midway3-login3.rcc.local>
2024-10-07 13:40:46 -0500

    final ala3 results

---
+++ aib9_vacuum_mtd_1d/qsubmit.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+ 
+#SBATCH --job-name=aib9_vacuum_mtd
+#SBATCH --output=%x.out
+#SBATCH --error=%x.err
+
+#SBATCH --time=2-0:00:00
+ 
+#SBATCH --partition=dinner
+#SBATCH --account=pi-dinner
+ 
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=20G
+ 
+#SBATCH --export=NONE
+
+echo $SLURM_JOB_NAME
+echo $SLURM_JOB_NODELIST
+
+rm -f analysis.* \#md_0_1.* bck.*
+gmx_mpi grompp -f md.mdp -c em.gro -p topol.top -o md_0_1.tpr
+gmx_mpi mdrun -deffnm md_0_1 -plumed plumed.dat
