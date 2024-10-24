80e5fe86e5bc5b49d4253c84020a6cc97f4a03bf
Nils Strand <nstrand@midway3-login3.rcc.local>
2024-10-24 15:06:34 -0500

    switching back to Gaussian kde

--- trp3_mtd/qsubmit2.sh
+++ trp3_mtd/qsubmit2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
  
-#SBATCH --job-name=ala3_mtd
+#SBATCH --job-name=trp3_histo
 #SBATCH --output=%x.out
 #SBATCH --error=%x.err
 
@@ -10,17 +10,15 @@
 #SBATCH --account=pi-dinner
  
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=4
+#SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=50G
  
 #SBATCH --export=NONE
 
-export OMP_NUM_THREADS=8
+export PLUMED_NUM_THREADS=8
 
 echo $SLURM_JOB_NAME
 echo $SLURM_JOB_NODELIST
 
-./setup.sh
-mpirun -np 4 gmx_mpi mdrun -deffnm md_0_1 -plumed plumed.dat -multidir phi2 psi2 phi3 psi3 -replex 2000
 ./sumhills.sh
