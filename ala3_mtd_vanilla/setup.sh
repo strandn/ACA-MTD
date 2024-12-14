baaae02dcbc1c53885838a421db15d6d12308916
Nils Strand <nstrand@midway3-login3.rcc.local>
2024-12-14 13:38:33 -0600

    several new jobs done

---
+++ ala3_mtd_vanilla/setup.sh
@@ -0,0 +1,16 @@
+cp ala3.pdb em.gro md.mdp topol.top phi2
+cd phi2
+gmx_mpi grompp -f md.mdp -c em.gro -p topol.top -o md_0_1.tpr
+cd ..
+cp ala3.pdb em.gro md.mdp topol.top psi2
+cd psi2
+gmx_mpi grompp -f md.mdp -c em.gro -p topol.top -o md_0_1.tpr
+cd ..
+cp ala3.pdb em.gro md.mdp topol.top phi3
+cd phi3
+gmx_mpi grompp -f md.mdp -c em.gro -p topol.top -o md_0_1.tpr
+cd ..
+cp ala3.pdb em.gro md.mdp topol.top psi3
+cd psi3
+gmx_mpi grompp -f md.mdp -c em.gro -p topol.top -o md_0_1.tpr
+cd ..
