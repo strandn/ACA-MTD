8a14f0a1375d529e6f84f89e11d232c0b5793a29
Nils Strand <nstrand@midway3-login3.rcc.local>
2025-01-08 23:59:29 -0600

    new results

---
+++ ala4_60_30_3d_30_40/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+for i in `seq 0 39`;
+do
+    rm -rf $i
+    mkdir $i
+done
---
+++ ala4_80_40_3d_30_40/qsubmit2.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+ 
+#SBATCH --job-name=ala4_histo
+#SBATCH --output=%x.out
+#SBATCH --error=%x.err
+
+#SBATCH --time=2-0:00:00
+ 
+#SBATCH --partition=dinner
+#SBATCH --account=pi-dinner
+ 
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=20G
+ 
+#SBATCH --export=NONE
+
+echo $SLURM_JOB_NAME
+echo $SLURM_JOB_NODELIST
+
+export PLUMED_NUM_THREADS=8
+
+sed "14,1250053d" colvar.dat > colvar_half.dat
+plumed driver --plumed plumed3.dat --noatoms
