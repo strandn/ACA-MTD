6b40ba80e2103841b05f835c6842afc728909d38
Nils Strand <nstrand@midway3-login3.rcc.local>
2025-01-21 09:15:21 -0600

    some ala4 results

--- ala4_60_30_3d_20/setup_restart.sh
+++ ala4_60_30_3d_20/setup_restart.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-sed -i '$d' */colvar.*
-for i in `seq 0 39`; do sed '/#!/d' $i/colvar.$i.dat > $i/colvar.$i.dat.0; done
+for i in `seq 0 39`;
+do
+    sed '$d' $i/colvar.$i.dat > $i/colvar.$i.dat.0
+    sed -i '/#!/d' $i/colvar.$i.dat.0
+done
 python3 merge_colvars.py
