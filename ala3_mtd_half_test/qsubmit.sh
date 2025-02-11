9e102e1298527cda292613d91931b338b4f6a5e9
Nils Strand <nstrand@midway3-login3.rcc.local>
2025-02-10 18:14:07 -0600

    ala3 with sump

---
+++ ala3_mtd_half_test/qsubmit.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+ 
+#SBATCH --job-name=ala3_mtd
+#SBATCH --output=%x.out
+#SBATCH --error=%x.err
+
+#SBATCH --time=2-0:00:00
+ 
+#SBATCH --partition=dinner
+#SBATCH --account=pi-dinner
+ 
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=50G
+ 
+#SBATCH --export=NONE
+
+echo $SLURM_JOB_NAME
+echo $SLURM_JOB_NODELIST
+
+export PLUMED_NUM_THREADS=8
+
+rm -f *ff* \#md_0_1.* bck.*
+gmx_mpi grompp -f md.mdp -c em.gro -p topol.top -o md_0_1.tpr
+gmx_mpi mdrun -deffnm md_0_1 -plumed plumed.dat
+plumed driver --plumed plumed2.dat --noatoms
+sed "10,250009d" COLVAR > COLVAR.0
+sed "10,5000009d" COLVAR > COLVAR.1
+plumed driver --plumed plumed3.dat --noatoms
+plumed driver --plumed plumed4.dat --noatoms
