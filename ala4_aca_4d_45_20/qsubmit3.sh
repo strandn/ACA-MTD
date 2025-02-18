c87c1b2b6638a5254fb0141d510585deea188c6a
Nils Strand <nstrand@midway3-login3.rcc.local>
2025-02-18 14:55:55 -0600

    lots of histos for ala4 4d aca

---
+++ ala4_aca_4d_45_20/qsubmit3.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+ 
+#SBATCH --job-name=ala4_histo
+#SBATCH --output=%x.out
+#SBATCH --error=%x.err
+
+#SBATCH --time=2-0:00:00
+ 
+#SBATCH --partition=dinner
+#SBATCH --account=pi-dinner
+ 
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=20G
+ 
+#SBATCH --export=NONE
+
+echo $SLURM_JOB_NAME
+echo $SLURM_JOB_NODELIST
+
+plumed driver --plumed plumed7.dat --noatoms
